8ce7f5835b2dfb74d6479735b76614adef0db503
Yue-plus <Yue_plus@foxmail.com>
2022-11-13 20:20:46 +0800

    from JVTC.Yue.Zone

---
+++ README.md
@@ -0,0 +1,5 @@
+# [Solution.JVTC.Yue.Zone](https://JVTC.Yue.Zone/)
+
+本仓库是 [JVTC.Yue.Zone](https://JVTC.Yue.Zone/) 的一部分。
+
+为方便管理还是分两个仓库。
---
+++ src/test/java/zone/yue/jvtc/solution/WeeklyWork/Y22W37Test.java
@@ -0,0 +1,16 @@
+package zone.yue.jvtc.solution.WeeklyWork;
+
+import org.junit.jupiter.api.*;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class Y22W37Test {
+    @BeforeAll
+    static void testY22W37BeforeAll() {
+        System.out.println("----- 2022 年第 37 周编程作业 测试开始 -----");
+    }
+
+    @AfterAll
+    static void testY22W37AfterAll() {
+        System.out.println("===== 2022 年第 37 周编程作业 测试结束 =====");
+    }
+}
