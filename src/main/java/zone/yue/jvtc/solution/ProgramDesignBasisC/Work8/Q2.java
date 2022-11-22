c9849a6bfdffa1364a990db7ccb28165faeba8db
Yue-plus <Yue_plus@foxmail.com>
2022-11-22 20:36:50 +0800

    update ProgramDesignBasisC.Work8.Q2

--- src/main/java/zone/yue/jvtc/solution/ProgramDesignBasisC/Work8/Q2.java
+++ src/main/java/zone/yue/jvtc/solution/ProgramDesignBasisC/Work8/Q2.java
@@ -2,14 +2,16 @@ package zone.yue.jvtc.solution.ProgramDesignBasisC.Work8;
 
 
 /**
+ * 求 1~100 内的素数，并输出。
+ *
  * @author Yue_plus
  */
 public class Q2 {
     public static void main(String[] args) {
         // 100 以内的素数
-        int[] pn = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+        short[] pn = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
 
-        for (int n: pn) {
+        for (short n: pn) {
             System.out.println(n);
         }
     }
