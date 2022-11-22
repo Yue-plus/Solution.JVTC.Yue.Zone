ce5a0f7df35d669c3c15c2827fea1c1f2b9ddca4
Yue-plus <Yue_plus@foxmail.com>
2022-11-22 20:35:54 +0800

    add ProgramDesignBasisC.Work8.Q2

---
+++ src/main/java/zone/yue/jvtc/solution/ProgramDesignBasisC/Work8/Q2.java
@@ -0,0 +1,16 @@
+package zone.yue.jvtc.solution.ProgramDesignBasisC.Work8;
+
+
+/**
+ * @author Yue_plus
+ */
+public class Q2 {
+    public static void main(String[] args) {
+        // 100 以内的素数
+        int[] pn = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+
+        for (int n: pn) {
+            System.out.println(n);
+        }
+    }
+}
