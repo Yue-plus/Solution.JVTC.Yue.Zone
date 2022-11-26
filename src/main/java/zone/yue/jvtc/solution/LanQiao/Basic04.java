05852123de1d0ad9c295aaf68d0cdce3ad1de683
Yue-plus <Yue_plus@foxmail.com>
2022-11-26 19:10:38 +0800

    done LanQiao Basic04

--- src/main/java/zone/yue/jvtc/solution/LanQiao/Basic04.java
+++ src/main/java/zone/yue/jvtc/solution/LanQiao/Basic04.java
@@ -1,6 +1,5 @@
 package zone.yue.jvtc.solution.LanQiao;
 
-import java.math.BigInteger;
 import java.util.Scanner;
 
 /**
@@ -10,8 +9,22 @@ import java.util.Scanner;
  */
 public class Basic04 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        long n = sc.nextLong();
-        // TODO:
+        int f1 = 1;
+        int f2 = 1;
+
+        int n = new Scanner(System.in).nextInt();
+        if (n < 3) {
+            System.out.println(1);
+            return;
+        }
+
+        int f3 = 0;
+        for (int i = 3; i <= n; i++) {
+            //第三个数等于前两个数之和
+            f3 = (f1 + f2) % 10007;
+            f1 = f2;
+            f2 = f3;
+        }
+        System.out.println(f3);
     }
 }
