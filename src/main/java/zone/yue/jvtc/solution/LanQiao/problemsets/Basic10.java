f8ca50c6a60d930e1539d5577c94793193ebab72
Yue-plus <Yue_plus@foxmail.com>
2022-12-25 10:31:12 +0800

    fix LanQiao Basic10

--- src/main/java/zone/yue/jvtc/solution/LanQiao/problemsets/Basic10.java
+++ src/main/java/zone/yue/jvtc/solution/LanQiao/problemsets/Basic10.java
@@ -1,6 +1,5 @@
 package zone.yue.jvtc.solution.LanQiao.problemsets;
 
-import java.math.BigInteger;
 import java.util.Scanner;
 
 /**
@@ -10,8 +9,6 @@ import java.util.Scanner;
  */
 public class Basic10 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        BigInteger n = sc.nextBigInteger();
-        System.out.println(n.toString(16));
+        System.out.println(new Scanner(System.in).nextBigInteger().toString(16).toUpperCase());
     }
 }
